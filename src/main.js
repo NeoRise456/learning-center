2f66f89d7f4194e5b0fe35b33eb3b1a8116f0004
neo <neorise456@gmail.com>
2024-09-12 16:12:14 -0500

    feat(ui): setup prime vue components globally.

--- src/main.js
+++ src/main.js
@@ -2,4 +2,60 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import ConfirmationService from "primevue/confirmationservice";
+import ToastService from "primevue/toastservice";
+import DialogService from "primevue/dialogservice";
+import Button from "primevue/button";
+import Card from "primevue/card";
+import Column from "primevue/column";
+import ConfirmDialog from "primevue/confirmdialog";
+import DataTable from "primevue/datatable";
+import Checkbox from "primevue/checkbox";
+import Dialog from "primevue/dialog";
+import Select from "primevue/select";
+import InputText from "primevue/inputtext";
+import FileUpload from "primevue/fileupload";
+import FloatLabel from "primevue/floatlabel";
+import IconField from "primevue/iconfield";
+import InputNumber from "primevue/inputnumber";
+import Menu from "primevue/menu";
+import Rating from "primevue/rating";
+import Row from "primevue/row";
+import Drawer from "primevue/drawer";
+import Toast from "primevue/toast";
+import Tag from "primevue/tag";
+import Toolbar from "primevue/toolbar";
+
+const app = createApp(App);
+app.use(PrimeVue, { ripple: true ,theme: 'aura' });
+
+app.use(ConfirmationService)
+    .use(DialogService)
+    .use(ToastService);
+
+app.component('pv-button', Button)
+    .component('pv-card', Card)
+    .component('pv-column', Column)
+    .component('pv-confirm-dialog', ConfirmDialog)
+    .component('pv-checkbox', Checkbox)
+    .component('pv-data-table', DataTable)
+    .component('pv-dialog', Dialog)
+    .component('pv-select' , Select)
+    .component('pv-input-text', InputText)
+    .component('pv-file-upload', FileUpload)
+    .component('pv-float-label', FloatLabel)
+    .component('pv-icon-field', IconField)
+    .component('pv-input-number', InputNumber)
+    .component('pv-menu', Menu)
+    .component('pv-rating', Rating)
+    .component('pv-row', Row)
+    .component('pv-drawer', Drawer)
+    .component('pv-toast', Toast)
+    .component('pv-tag', Tag)
+    .component('pv-toolbar', Toolbar);
+
+app.mount('#app');
