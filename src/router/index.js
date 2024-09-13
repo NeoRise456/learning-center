544986db4e34d05e0bcfa5c05e604794b4811194
neo <neorise456@gmail.com>
2024-09-13 16:33:38 -0500

    feat(in-app-navigation): configure navigation routes.

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,20 @@
 import {createRouter} from "vue-router";
+import HomeComponent from "../public/pages/home.component.vue";
+import AboutComponent from "../public/pages/about.component.vue";
 
 const router = createRouter({
     history: createWebHashHistory(),
-    routes: []
+    routes: [
+        { path: '/home', name:'home',component: HomeComponent, meta: {title: 'Home'} },
+        { path: '/about', name:'about',component: AboutComponent, meta: {title: 'About'} },
+        { path: '/', redirect: '/home' }
+    ]
+});
+
+router.beforeEach((to, from, next) => {
+    let baseTitle = 'ACME Learning Center';
+    document.title = `${baseTitle} | ${to.meta['title']}`;
+    next();
 });
 
 export default router;
