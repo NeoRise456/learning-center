675d315e9beb9c14f1e9b19d3d5b2a00f238a2a7
neo <neorise456@gmail.com>
2024-09-19 16:48:43 -0500

    zzzzzz

---
+++ src/publishing/pages/category-management.component.vue
@@ -0,0 +1,28 @@
+<script>
+import {Category} from "../model/category.entity.js";
+
+export default {
+  name: "category-management",
+  data(){
+    return{
+      title: {singular: "Category", plural: "Categories"},
+      categories: [],
+      category: new Category({}),
+      selectedCategories: [],
+      categoryService:null,
+      createAndEditDialogVisible: false,
+      isEdit: false,
+      submitted: false
+    }
+  }
+
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
