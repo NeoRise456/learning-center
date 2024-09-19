94bc2f30949184e50b662b338c463079ee2a7742
neo <neorise456@gmail.com>
2024-09-19 15:57:43 -0500

    chore:  addmonsesaso

--- src/public/pages/about.component.vue
+++ src/public/pages/about.component.vue
@@ -6,7 +6,7 @@ export default {
 
 <template>
   <div class="w-full">
-    <h1> About us monse</h1>
+    <h1> About us mose</h1>
     <p> ACME Learning Center is an Education Buissness Platform part of ACME Corporation</p>
   </div>
 </template>
